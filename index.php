8b4c4dbdf35d42b6df2961aad88ecba1ce49f5bd
Saadia <saadiarehman25@gmail.com>
2020-07-27 23:14:15 +0500

    simple variable echoing

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+// Variables define with $variableName
+$firstName='Omer';
+$secondName = 'Farooq';
+//one way 
+//echo "Hello , {$firstName} {$secondName} ";
+//second way
+echo 'Hello , '.$firstName.$secondName;
+?>
