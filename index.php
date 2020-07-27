5143a7abd73c25f17f406a44f0268eca5b117fe3
Saadia <saadiarehman25@gmail.com>
2020-07-28 00:16:52 +0500

    PHP with HTML

--- index.php
+++ index.php
@@ -14,30 +14,30 @@
     </style>
 </head>
 <body>
-         Simple Html way
+        // Simple Html way
         <h1>Hello World</h1>
         
 
-         same work with using PHP 
+        // same work with using PHP 
             <?= "<h1>Hello World</h1> " ?>
 
-         same work by creating variable
+         //same work by creating variable
         <br>
         <?php
             $document = 'Hello World';
             echo "<h1>{$document}</h1>"; 
         ?>
-            fetch data from url http://localhost:8888/?name=john -----name is key, john is value
+          //  fetch data from url http://localhost:8888/?name=john -----name is key, john is value
         <?php
                 $name= $_GET['name'];
                 echo    "<h1>Hello {$name}</h1>" ;
         ?>
 
-        any tag can apply in url like this
+        //any tag can apply in url like this
                             localhost:8888/?name=<a href="https://www.google.com">JOHN</a>
         
 
-        we can get value of url using following function
+        //we can get value of url using following function
         <br>
 
         <br>
