24c2a962e9b8f4204eb783b24b0f131be13a73ef
Saadia <saadiarehman25@gmail.com>
2020-07-29 00:22:27 +0500

    create class of tasks display completed tasks line over them

--- index.php
+++ index.php
@@ -1,12 +1,27 @@
 <?php
-// Learn how to display boolean using ternary operators $ capitilize first words using built in function
-// Learn doing above taskusing if/else statement
+//create class named as task ,initiate class with multiple objects through array, display completed taska as line over them 
+require 'functions.php';
+class Task {
+    public $description;
+    public $completed = false;
+    public function __construct($description){
+        $this->description=$description;
+    }
+    public function isComplete(){
+        return $this->completed;
+    }
+    public function complete(){
+        $this->completed=true;
+    }
+}
 $tasks = [
-    'title' => 'revision',
-    'due' => '31st of july',
-    'assign_to' => 'saadia',
-    'completed' =>  true
+    new Task('Go to store'),
+    new Task('Finish screencast'),
+    new Task('clean my room')
 ];
+
+$tasks[0]->complete();
+
 require 'index.view.php';
 
 ?>
--- index.view.php
+++ index.view.php
@@ -4,38 +4,19 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <style>
-        h1{
-            background-color:grey;
-            padding:20px;
-            text-align:center;
-        }
-         li{
-            list-style-type:none;
-        }
-    
-    </style>
 </head>
 <body>
-                        <h1>My Task Today</h1>
-        <ul>
-                <li>
-                   <strong><?="Task : " ?></strong> <?= $tasks['title']; ?>
-                </li>
-                <li>
-                   <strong><?="Due Date : " ?></strong> <?= $tasks['due']; ?>
-                </li>
-                <li>
-                   <strong><?="Assigned By : " ?></strong> <?= $tasks['assign_to']; ?>
-                </li>
-                <li>
-                   <strong><?="Status : " ?></strong> 
-                     <?php if ($tasks['completed']):?>
-                        <?= "finished"; ?>
-                     <?php else: ?>
-                        <?= "Incomplete"; ?>
-                     <?php endif; ?> 
-                </li>            
-        </ul>
+    <ul>
+          <?php foreach($tasks as $task): ?>
+            <li>
+              <?php if($task->isComplete()): ?>
+                 <strike><?= $task->description; ?></strike> 
+                <?php else: ?>
+                <?= $task->description; ?>
+              <?php endif; ?>
+             </li>
+             
+          <?php endforeach; ?>
+    </ul>
 </body>
 </html>
