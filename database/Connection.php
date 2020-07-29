4b05ad9832de083c9c6ab9a22b03a52c33d97b56
Saadia <saadiarehman25@gmail.com>
2020-07-29 15:55:31 +0500

    Refractoring to minimize code

---
+++ database/Connection.php
@@ -0,0 +1,17 @@
+<?php
+
+class Connection{
+
+    public static function make(){
+        try{
+            return new PDO('mysql:host=127.0.0.1;dbname=mytodo',"root","");
+        }
+        Catch(PDOException $e){
+            echo($e->getMessage());
+        }
+    
+    }
+
+}
+
+?>
--- index.php
+++ index.php
@@ -1,11 +1,8 @@
 <?php
-//create class named as task ,initiate class with multiple objects through array, display completed taska as line over them 
-require 'functions.php';
-require 'Task.php';
-$pdo = connecttoDB();
-$tasks = fetchAll($pdo);
 
+$query=require 'bootstrap.php';
 
+$tasks= $query->selectAll('todos');
 
 require 'index.view.php';
 
