fae0a6660eab30217e731d9d6a9cc81144bc6c2f
Saadia <saadiarehman25@gmail.com>
2020-07-29 14:47:00 +0500

    fetch data from database and display it

--- functions.php
+++ functions.php
@@ -1,5 +1,26 @@
 <?php
 
+function connecttoDB(){
+    try{
+        return new PDO('mysql:host=127.0.0.1;dbname=mytodo',"root","");
+    }
+    Catch(PDOException $e){
+        echo($e->getMessage());
+    }
+    
+}
+
+
+function fetchAll($pdo){
+
+    $statement=$pdo->prepare("select * from todos");
+
+    $statement->execute();
+
+    return $statement->fetchAll(PDO::FETCH_CLASS, 'Task');
+
+}
+
 function dd($data){
     echo "<pre>";
     die(var_dump($data));
--- index.php
+++ index.php
@@ -1,26 +1,11 @@
 <?php
 //create class named as task ,initiate class with multiple objects through array, display completed taska as line over them 
 require 'functions.php';
-class Task {
-    public $description;
-    public $completed = false;
-    public function __construct($description){
-        $this->description=$description;
-    }
-    public function isComplete(){
-        return $this->completed;
-    }
-    public function complete(){
-        $this->completed=true;
-    }
-}
-$tasks = [
-    new Task('Go to store'),
-    new Task('Finish screencast'),
-    new Task('clean my room')
-];
+require 'Task.php';
+$pdo = connecttoDB();
+$tasks = fetchAll($pdo);
 
-$tasks[0]->complete();
+
 
 require 'index.view.php';
 
